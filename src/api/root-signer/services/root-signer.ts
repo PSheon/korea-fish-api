4770e2c35235e27f08a4985c88860714831e5b55
PSheon <pauljiang61020@gmail.com>
2024-02-05 13:00:03 +0800

    feat: updated app.

--- src/api/root-signer/services/root-signer.ts
+++
@@ -1,7 +0,0 @@
-/**
- * root-signer service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::root-signer.root-signer');
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { moralisBootstrap } from "./bootstrap/moralis";
+
 export default {
   /**
    * An asynchronous register function that runs before
@@ -14,5 +16,8 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  async bootstrap({ strapi }) {
+    /* Moralis Bootstrap */
+    await moralisBootstrap(strapi);
+  },
 };
